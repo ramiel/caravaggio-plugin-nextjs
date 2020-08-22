0ec8b5f039b9ede4ddc7e5ed41ba7a406683c10f
Fabrizio Ruggeri <fabrizio.ruggeri@gmail.com>
2020-08-22 12:05:10 +0200

    Remove some useless check on every request

--- src/index.ts
+++ src/index.ts
@@ -10,23 +10,25 @@ interface NextJSPluginOptions {
 const nextJSPlugin: PluginConstructor<NextJSPluginOptions> = (opt) => {
   const { pluginOptions: { baseUrl } = {} } = opt;
 
+  let base: string;
+  if (baseUrl) {
+    base = baseUrl;
+  } else {
+    if (process.env.VERCEL_URL) {
+      base =
+        process.env.NODE_ENV === 'production'
+          ? `https://${process.env.VERCEL_URL}`
+          : `http://${process.env.VERCEL_URL}`;
+    }
+  }
+
   const plugin: Plugin = {
     urlTransform: async (url, req) => {
-      let base: string;
-      if (baseUrl) {
-        base = baseUrl;
-      } else {
-        if (process.env.VERCEL_URL) {
-          base =
-            process.env.NODE_ENV === 'production'
-              ? `https://${process.env.VERCEL_URL}`
-              : `http://${process.env.VERCEL_URL}`;
-        } else {
-          base =
-            process.env.NODE_ENV === 'production'
-              ? `https://${req.headers.host}`
-              : `http://${req.headers.host}`;
-        }
+      if (!base) {
+        base =
+          process.env.NODE_ENV === 'production'
+            ? `https://${req.headers.host}`
+            : `http://${req.headers.host}`;
       }
       if (url.startsWith('/')) {
         const final = new URL(url, base).toString();
